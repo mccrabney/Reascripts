6eb69e9a9c4e54cabbde9f6941584dcfacd33f6f
mccrabney <lewis.scaife@gmail.com>
2022-12-17 15:00:15 -0500

    Add files via upload

---
+++ Razor Edits/mccrabney_razor edits_delete notes contained in RE.lua
@@ -0,0 +1,31 @@
+--[[
+ * ReaScript Name: delete notes contained within Razor Edit
+ * Author: mccrabney
+ * Licence: GPL v3
+ * REAPER: 6.0
+ * Extensions: None
+ * Version: 1.0
+--]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2021-04-02)
+   + Initial Release
+--]]
+
+  for key in pairs(reaper) do _G[key]=reaper[key]  end 
+  
+  local info = debug.getinfo(1,'S');
+  script_path = info.source:match([[^@?(.*[\/])[^\/]-$]]):match('(.*)\\')
+  dofile(script_path .. "/mccrabney_razor edits_Razor Edit Control Functions.lua")    
+  ----------------------------  
+  function main()
+    reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+    task = 8
+    job = 1
+    SetGlobalParam(job, task, _)
+    reaper.Undo_EndBlock("Delete Notes Contained in Razer Edits", 0) -- End of the undo block. Leave it at the bottom of your main function.
+  end
+  ----------------------------
+  defer(main)
+
---
+++ Razor Edits/mccrabney_razor edits_delete notes lower than last-hit note in RE.lua
@@ -0,0 +1,32 @@
+--[[
+ * ReaScript Name: delete notes lower than last-hit note in Razor Edit
+ * Author: mccrabney
+ * Licence: GPL v3
+ * REAPER: 6.0
+ * Extensions: None
+ * Version: 1.0
+--]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2021-04-02)
+   + Initial Release
+--]]
+
+
+  for key in pairs(reaper) do _G[key]=reaper[key]  end 
+  
+  local info = debug.getinfo(1,'S');
+  script_path = info.source:match([[^@?(.*[\/])[^\/]-$]]):match('(.*)\\')
+  dofile(script_path .. "/mccrabney_razor edits_Razor Edit Control Functions.lua")    
+  ----------------------------  
+  function main()
+    reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+    task = 3
+    job = 1
+    SetGlobalParam(job, task, _)
+    reaper.Undo_EndBlock("Delete Notes << Last-Hit Note in Razer Edits", 0) -- End of the undo block. Leave it at the bottom of your main function.
+  end
+  ----------------------------
+  defer(main)
+
