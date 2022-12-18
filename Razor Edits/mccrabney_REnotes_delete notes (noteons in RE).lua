519c1d714099d5e967d3d50953f0d556d2a8fd2f
mccrabney <lewis.scaife@gmail.com>
2022-12-18 10:07:19 -0500

    Add files via upload

---
+++ Razor Edits/mccrabney_REnotes_delete notes (noteons in RE).lua
@@ -0,0 +1,32 @@
+--[[
+ * ReaScript Name: delete notes whose note-ons are contained in Razor Edit
+ * Author: mccrabney
+ * Licence: GPL v3
+ * REAPER: 6.0
+ * Extensions: None
+ * Version: 1.0
+--]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2021-04-02)
+   + Initial Release
+--]]
+
+  for key in pairs(reaper) do _G[key]=reaper[key]  end 
+  
+  local info = debug.getinfo(1,'S');
+  script_path = info.source:match([[^@?(.*[\/])[^\/]-$]]):match('(.*)\\')
+  dofile(script_path .. "/mccrabney_Razor Edit Control Functions.lua")    
+  ----------------------------  
+  function main()
+    
+    task = 1
+    job = 1
+    SetGlobalParam(job, task, _)
+    
+  end
+  ----------------------------
+  defer(main)
+  
+
