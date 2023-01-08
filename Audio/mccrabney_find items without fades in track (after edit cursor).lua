ba73762cf18a1950d4a3645418cf0483a0aa89ab
mccrabney <lewis.scaife@gmail.com>
2023-01-08 15:31:05 -0500

    Add files via upload

---
+++ Audio/mccrabney_find items without fades in track (after edit cursor).lua
@@ -0,0 +1,64 @@
+--[[
+ * ReaScript Name: find items without fades in track
+ * Author: mccrabney
+ * Licence: GPL v3
+ * REAPER: 6.0
+ * Extensions: None
+ * Version: 1.0
+--]]
+ 
+--[[
+ * Changelog:
+   
+ * v1.0 (2022-01-01)
+   + Initial Release
+--]]
+
+
+------------------------------------------------------
+local function no_undo()reaper.defer(function()end)end
+-------------------------------------------------------
+local CountTrack =  reaper.CountSelectedTracks(0)
+local track = reaper.GetSelectedTrack( 0, CountTrack-1 )
+local CountTrItem = reaper.CountTrackMediaItems(track)
+---------------------------------------------------------------------
+if CountTrack   == 0 then no_undo() return end  -- if no tracks or items, just give it up and quit
+if CountTrItems == 0 then no_undo() return end  
+local cursorPos = reaper.GetCursorPosition()
+
+local namedCommand = reaper.NamedCommandLookup("_SWS_UNSELONTRACKS")  -- unselect all item
+reaper.Main_OnCommand(namedCommand, 0)
+
+reaper.Undo_BeginBlock()
+
+for i = 0, CountTrItem-1 do         -- for each item,               
+  local item = reaper.GetTrackMediaItem(track,i)      
+  local take = reaper.GetActiveTake(item)
+  local isMIDI = reaper.TakeIsMIDI(take)
+    
+  if isMIDI ~= true then                 -- if take is not MIDI
+    local fadeIn = reaper.GetMediaItemInfo_Value( item, 'D_FADEINLEN' )
+    local fadeOut = reaper.GetMediaItemInfo_Value( item, 'D_FADEOUTLEN' )
+    local autoFadeIn = reaper.GetMediaItemInfo_Value( item, 'D_FADEINLEN_AUTO' )
+    local autoFadeOut = reaper.GetMediaItemInfo_Value( item, 'D_FADEOUTLEN_AUTO' )
+    
+    local itemStart = reaper.GetMediaItemInfo_Value( item, 'D_POSITION' )
+    local itemEnd = itemStart + reaper.GetMediaItemInfo_Value( item, 'D_LENGTH' )
+    
+    if cursorPos < itemStart then       -- if itemstart is later than edit cursor
+      if fadeIn == 0 and autoFadeIn == 0 then 
+        reaper.SetMediaItemSelected( item, 1 )
+        reaper.SetEditCurPos(itemStart, 1, 0)
+        reaper.Undo_EndBlock('find fadeless item' , -1)
+      return end
+      if fadeOut == 0 and autoFadeOut == 0 then
+        reaper.SetMediaItemSelected( item, 1 )
+        reaper.SetEditCurPos(itemEnd, 1, 0)
+        reaper.Undo_EndBlock('find fadeless item' , -1)
+      return end
+    end
+  end
+end
+
+reaper.UpdateArrange()
+reaper.Undo_EndBlock('no fadeless items found' , -1)
