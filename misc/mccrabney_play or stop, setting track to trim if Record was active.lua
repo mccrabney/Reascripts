61b2836bef05a032edecfd203f06a7765b4a2f2c
mccrabney <lewis.scaife@gmail.com>
2023-03-17 08:04:55 -0400

    Add files via upload

--- misc/mccrabney_play or stop, setting track to trim if Record was active.lua
+++ misc/mccrabney_play or stop, setting track to trim if Record was active.lua
@@ -4,33 +4,55 @@
  * Licence: GPL v3
  * REAPER: 6.0
  * Extensions: None
- * Version: 1.0
+ * Version: 1.1
 --]]
  
 --[[
+ * v1.1 (2023-03-17)
+   + nil fix
  * v1.0 (2023-02-15)
    + Initial Release
 --]]
 
+local automationMode 
 local selTrack = reaper.GetSelectedTrack(0,0);
-local automationMode = reaper.GetMediaTrackInfo_Value(selTrack, "I_AUTOMODE ")
+
+if selTrack ~= nil then 
+  automationMode = reaper.GetMediaTrackInfo_Value(selTrack, "I_AUTOMODE ")
+end
+
 local transportMode = reaper.GetPlayState()
 
 function Main()
-  
-  if transportMode == 5 then                                    -- if transport is recording
-  --  reaper.Undo_BeginBlock() 
-    reaper.SetMediaTrackInfo_Value(selTrack, "I_AUTOMODE", 0)   -- set selected track to Trim
-    reaper.Main_OnCommand(1016, 0)                              -- stop
-  elseif transportMode == 1 then                                -- if REAPER is playing,
-    --reaper.Main_OnCommand(1008, 0)                            -- pause
-    reaper.Main_OnCommand(1016, 0)                              -- stop
-    reaper.SetMediaTrackInfo_Value(selTrack, "I_AUTOMODE", 0)   -- set selected track to Trim
-  elseif transportMode == 0 then                                -- if REAPER is stopped,
-    reaper.Main_OnCommand(1007, 0)                              -- play
-  elseif transportMode == 2 then                                -- if REAPER is paused,
-    reaper.Main_OnCommand(1007, 0)                              -- play
-  end   
+ 
+  if selTrack == nil then
+    if transportMode == 5 then                                    -- if transport is recording
+    --  reaper.Undo_BeginBlock() 
+      reaper.Main_OnCommand(1016, 0)                              -- stop
+    elseif transportMode == 1 then                                -- if REAPER is playing,
+      --reaper.Main_OnCommand(1008, 0)                            -- pause
+      reaper.Main_OnCommand(1016, 0)                              -- stop
+    elseif transportMode == 0 then                                -- if REAPER is stopped,
+      reaper.Main_OnCommand(1007, 0)                              -- play
+    elseif transportMode == 2 then                                -- if REAPER is paused,
+      reaper.Main_OnCommand(1007, 0)                              -- play
+    end
+  end
+  if selTrack ~= nil then
+    if transportMode == 5 then                                    -- if transport is recording
+    --  reaper.Undo_BeginBlock() 
+      reaper.SetMediaTrackInfo_Value(selTrack, "I_AUTOMODE", 0)   -- set selected track to Trim
+      reaper.Main_OnCommand(1016, 0)                              -- stop
+    elseif transportMode == 1 then                                -- if REAPER is playing,
+      --reaper.Main_OnCommand(1008, 0)                            -- pause
+      reaper.Main_OnCommand(1016, 0)                              -- stop
+      reaper.SetMediaTrackInfo_Value(selTrack, "I_AUTOMODE", 0)   -- set selected track to Trim
+    elseif transportMode == 0 then                                -- if REAPER is stopped,
+      reaper.Main_OnCommand(1007, 0)                              -- play
+    elseif transportMode == 2 then                                -- if REAPER is paused,
+      reaper.Main_OnCommand(1007, 0)                              -- play
+    end
+  end
   
   reaper.UpdateArrange()
 end
--- misc/mccrabney_record media and latch selected track automation.lua
+++ misc/mccrabney_record media and latch selected track automation.lua
@@ -4,30 +4,47 @@
  * Licence: GPL v3
  * REAPER: 6.0
  * Extensions: None
- * Version: 1.0
+ * Version: 1.1
 --]]
  
 --[[
+ * v1.1 (2023-03-17)
+   + nil fix
  * v1.0 (2023-02-15)
    + Initial Release
 --]]
 
 local selTrack = reaper.GetSelectedTrack(0,0);
-local automationMode = reaper.GetMediaTrackInfo_Value(selTrack, "I_AUTOMODE ")
+
+if selTrack ~= nil then 
+  automationMode = reaper.GetMediaTrackInfo_Value(selTrack, "I_AUTOMODE ")
+end
+
 local transportMode = reaper.GetPlayState()
 
 function Main()
-  if transportMode ~= 5 then                                      -- if transport is not recording
-    if automationMode == 0 then                                   -- if track is Trim,
-      reaper.SetMediaTrackInfo_Value(selTrack, "I_AUTOMODE", 4)   -- set selected track to Latch
-      reaper.Main_OnCommand(1013, 0)                              -- record
-    else 
-      reaper.Main_OnCommand(1013, 0)                              -- record
-    end
-  else                                                            -- if REAPER is recording,
+
+  if selTrack == nil and transportMode ~= 5 then
+    reaper.Main_OnCommand(1013, 0)                              -- record
+  end
+  if selTrack == nil and transportMode == 5 then
     reaper.Main_OnCommand(1016, 0)                                -- stop
-    reaper.SetMediaTrackInfo_Value(selTrack, "I_AUTOMODE", 0)     -- set track back to Trim
-  end    
+  end
+  
+  if selTrack ~= nil then
+    if transportMode ~= 5 then                                      -- if transport is not recording
+      if automationMode == 0 then                                   -- if track is Trim,
+        reaper.SetMediaTrackInfo_Value(selTrack, "I_AUTOMODE", 4)   -- set selected track to Latch
+        reaper.Main_OnCommand(1013, 0)                              -- record
+      else 
+        reaper.Main_OnCommand(1013, 0)                              -- record
+      end
+    else                                                            -- if REAPER is recording,
+      reaper.Main_OnCommand(1016, 0)                                -- stop
+      reaper.SetMediaTrackInfo_Value(selTrack, "I_AUTOMODE", 0)     -- set track back to Trim
+    end
+  end
+  
 end
 
 reaper.defer(Main) 
