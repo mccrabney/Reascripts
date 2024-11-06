00af019cafaabe75f3dee6ee5a34bc634befae29
mccrabney <lewis.scaife@gmail.com>
2024-11-06 06:14:20 -0500

    Add files via upload

--- MIDI Edits/mccrabney_Fiddler - set start or end of target notes to mouse cursor.lua
+++ MIDI Edits/mccrabney_Fiddler - set start or end of target notes to mouse cursor.lua
@@ -4,12 +4,15 @@
  * Licence: GPL v3
  * REAPER: 7.0
  * Extensions: None
- * Version: 1.00
+ * Version: 1.01
 --]]
  
 --[[
  * Changelog:
+ * v1.01 (2024-11-4)
+  * improve conditions when changed notes overlap with notes of the same pitch
  * v1.00 (2024-11-4)
+ 
 --]]
 
 --[[
@@ -53,8 +56,8 @@ function getNotesUnderMouseCursor()
   tableSize = tonumber(reaper.GetExtState(extName, 2 ))
   guidString = reaper.GetExtState(extName, 3 )
   take = reaper.SNM_GetMediaItemTakeByGUID( 0, guidString )
-  targetNoteNumber = tonumber(reaper.GetExtState(extName, 4 ))
   targetNoteIndex = tonumber(reaper.GetExtState(extName, 5 ))
+  targetNoteNumber = tonumber(reaper.GetExtState(extName, 4 ))
   cursorSource = tonumber(reaper.GetExtState(extName, 8 ))
   
   if tableSize ~= nil then 
@@ -68,11 +71,11 @@ function getNotesUnderMouseCursor()
     end
   end
   
-  return take, targetNoteNumber, targetNoteIndex
+  return take, targetNoteNumber, targetNoteIndex, numTargets
 end
 
 function getMouseInfo()    
-  local item, position_ppq, take, note
+  local item, mouseposppq, take, note
   window, _, details = reaper.BR_GetMouseCursorContext() -- initialize cursor context
   local mouse_pos = reaper.BR_GetMouseCursorContext_Position() -- get mouse position
   
@@ -81,19 +84,16 @@ function getMouseInfo()
   
     if reaper.TakeIsMIDI(take) then -- is take MIDI?
       item = reaper.BR_GetMouseCursorContext_Item() -- get item under mouse
-      position_ppq = reaper.MIDI_GetPPQPosFromProjTime(take, mouse_pos) -- convert to PPQ
-      local notesCount, _, _ = reaper.MIDI_CountEvts(take) -- count notes in current take
-      
+      mouseposppq = math.floor(reaper.MIDI_GetPPQPosFromProjTime(take, mouse_pos)) -- convert to PPQ
+      notesCount, _, _ = reaper.MIDI_CountEvts(take) -- count notes in current take
       for n = notesCount-1, 0, -1 do
         _, _, _, startppq, endppq, _, pitch, _ = reaper.MIDI_GetNote(take, n) -- get note start/end position              
-      
-        if startppq <= position_ppq and endppq >= position_ppq then 
-          note = pitch
-        end
+
       end
     end 
   end     
-  return note, take, item, position_ppq
+  
+  return take, item, mouseposppq, notesCount
 end
 
 ---------------------------------------------------------------------
@@ -102,11 +102,12 @@ end
     --]]------------------------------]]--
 
 function main()
+  reaper.ClearConsole()
   reaper.PreventUIRefresh(1)
   
   if RazorEditSelectionExists() then
   else
-    local note, take, selectedItem, position_ppq = getMouseInfo()
+    local take, selectedItem, mouseposppq, notesCount = getMouseInfo()
     take, targetNoteNumber, targetNoteIndex = getNotesUnderMouseCursor()
     
     if cursorSource == 0 and targetNoteNumber == nil then 
@@ -120,20 +121,87 @@ function main()
     end
     
     local pitchList = {"C_", "C#", "D_", "D#", "E_", "F_", "F#", "G_", "G#", "A_", "A#", "B_"}
-  
+    
     if take ~= nil and targetNoteIndex ~= -1 then
       _, _, _, startppqpos, endppqpos, _, pitch, _ = reaper.MIDI_GetNote( take, targetNoteIndex )
-      _, _, _, startppqposNext, _, _, pitchNext, _ = reaper.MIDI_GetNote( take, targetNoteIndex+1 )
       
-      if position_ppq ~= nil then 
-        if position_ppq > startppqpos then
-          curPos = reaper.MIDI_GetProjTimeFromPPQPos(take, startppqpos)
-          reaper.MIDI_SetNote( take, targetNoteIndex, nil, nil, nil, position_ppq, nil, nil, nil, nil)
-          reaper.SetEditCurPos(curPos, 1, 0)  -- set cursor to 1st noteon
-        else
-          curPos = reaper.MIDI_GetProjTimeFromPPQPos(take, position_ppq)
-          reaper.MIDI_SetNote( take, targetNoteIndex, nil, nil, position_ppq, nil, nil, nil, nil, nil)
-          reaper.SetEditCurPos(curPos, 1, 0)  -- set cursor to 1st noteon
+      if mouseposppq ~= nil then              -- if mouse is pointing at item
+        match = 0
+        for n = notesCount, 0, -1 do        -- for each note, last to first, get ppqs of the next/prev instances of note
+          if n ~= targetNoteIndex then
+            _, _, _, checkStart, checkEnd, _, checkPitch, _ = reaper.MIDI_GetNote(take, n) -- get note start/end position              
+            
+            if pitch == checkPitch and endppqpos < checkStart then -- if pitch matches target and its start is greater than target end
+              match = 1
+              --reaper.ShowConsoleMsg("next pitchmatch's start is past target end" .. "\n")
+              --reaper.ShowConsoleMsg("match n: " .. n .. "\n")
+              --reaper.ShowConsoleMsg("targetindex: " .. targetNoteIndex .. "\n")
+              startppqposNext = checkStart
+              --reaper.ShowConsoleMsg("startppqposNext: " .. startppqposNext .. "\n")
+            elseif pitch == checkPitch and startppqpos > checkEnd then -- if pitch matches target and end is before targetstart 
+              match = 1 
+              endppqposPrev = checkEnd
+              --reaper.ShowConsoleMsg("prev pitchmatch's end is before target start " .. "\n")
+              --reaper.ShowConsoleMsg("match n: " .. n .. "\n")
+              --reaper.ShowConsoleMsg("targetindex: " .. targetNoteIndex .. "\n")
+              --reaper.ShowConsoleMsg("endppqposPrev: " .. endppqposPrev .. "\n")
+              --reaper.ShowConsoleMsg("break loop" .."\n")
+              break
+            end
+          end
+        end
+        
+        --reaper.ShowConsoleMsg("startppqpos: " .. startppqpos .. "\n")
+        
+        if mouseposppq > startppqpos and startppqposNext ~= nil then     -- if mouse cursor is greater than notestart
+          --reaper.ShowConsoleMsg("\n" .. "mouse cursor is greater than notestart and there is a nextnote" .. "\n")
+          --reaper.ShowConsoleMsg("mousepos: " .. mouseposppq .. "\n")
+          --reaper.ShowConsoleMsg("startppqposNext: " .. startppqposNext .. "\n")
+          --reaper.ShowConsoleMsg("match= " .. match .. "\n")
+          if match ~= 1 then
+            reaper.MIDI_SetNote( take, targetNoteIndex, nil, nil, nil, mouseposppq, nil, nil, nil, nil)  -- set length to mousecursor
+          elseif mouseposppq < startppqposNext and match == 1 then   -- mouse cursor is before next pitchmatch
+            --reaper.ShowConsoleMsg("mouse cursor is before next pitchmatch" .. "\n")
+            reaper.MIDI_SetNote( take, targetNoteIndex, nil, nil, nil, mouseposppq, nil, nil, nil, nil)  -- set length to mousecursor
+          elseif mouseposppq > startppqposNext and match == 1 then  -- mouse cursor is past next note start
+            --reaper.ShowConsoleMsg("mouse cursor is past next note start" .. "\n")
+            reaper.MIDI_SetNote( take, targetNoteIndex, nil, nil, nil, startppqposNext-1, nil, nil, nil, nil)  -- set length to next note
+          end
+          curPos = reaper.MIDI_GetProjTimeFromPPQPos(take, startppqpos)  
+          reaper.SetEditCurPos(curPos, 1, 0)  -- set cursor to noteon
+        end
+        
+        if mouseposppq > startppqpos and startppqposNext == nil then  -- if no next note
+          --reaper.ShowConsoleMsg("mousepos: " .. mouseposppq .. "\n")
+          
+          --reaper.ShowConsoleMsg("\n" .. "mouse cursor is greater than notestart and there is no nextnote" .. "\n")
+          reaper.MIDI_SetNote( take, targetNoteIndex, nil, nil, nil, mouseposppq, nil, nil, nil, nil)  -- set length to next note
+          curPos = reaper.MIDI_GetProjTimeFromPPQPos(take, startppqpos)  
+          reaper.SetEditCurPos(curPos, 1, 0)  -- set cursor to noteon
+        end
+        
+        if mouseposppq < startppqpos and endppqposPrev ~= nil then     -- if mouse cursor is less than notestart
+          --reaper.ShowConsoleMsg("\n" .. "mouse cursor is less than notestart and there is a prevnote" .. "\n")
+          --reaper.ShowConsoleMsg("mousepos: " .. mouseposppq .. "\n")
+          --reaper.ShowConsoleMsg("endppqposPrev: " .. endppqposPrev .. "\n")
+          --reaper.ShowConsoleMsg("match= " .. match .. "\n")
+          
+          if mouseposppq > endppqposPrev then --
+            --reaper.ShowConsoleMsg("mouse cursor is past prev note end " .. "\n")
+            reaper.MIDI_SetNote( take, targetNoteIndex, nil, nil, mouseposppq, nil, nil, nil, nil, nil)
+          elseif mouseposppq < endppqposPrev then
+            --reaper.ShowConsoleMsg("mouse cursor is less than prev note end " .. "\n")
+            reaper.MIDI_SetNote( take, targetNoteIndex, nil, nil, endppqposPrev+1, nil, nil, nil, nil, nil)  -- set length to next note
+          end
+          curPos = reaper.MIDI_GetProjTimeFromPPQPos(take, startppqpos)  
+          reaper.SetEditCurPos(curPos, 1, 0)  -- set cursor to noteon
+        end
+  
+        if mouseposppq < startppqpos and endppqposPrev == nil then  -- if no prev note 
+          --reaper.ShowConsoleMsg("\n" .. "mouse cursor is earlier than notestart and there is no prevnote" .. "\n")
+          reaper.MIDI_SetNote( take, targetNoteIndex, nil, nil, mouseposppq, nil, nil, nil, nil, nil)  -- set length to next note
+          curPos = reaper.MIDI_GetProjTimeFromPPQPos(take, mouseposppq)  
+          reaper.SetEditCurPos(curPos, 1, 0)  -- set cursor to noteon
         end
       end
       
