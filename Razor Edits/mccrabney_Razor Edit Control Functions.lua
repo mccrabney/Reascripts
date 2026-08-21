51352b8e513ee3a2b8b1c41a4d5c054835773579
mccrabney <lewis.scaife@gmail.com>
2026-08-21 12:47:47 -0400

    Add files via upload

--- Razor Edits/mccrabney_Razor Edit Control Functions.lua
+++ Razor Edits/mccrabney_Razor Edit Control Functions.lua
@@ -2,14 +2,15 @@
  * ReaScript Name: Razor Edit Control Functions
  * Author: mccrabney
  * Licence: GPL v3
- * REAPER: 6.0
+ * REAPER: 7.0
  * Extensions: None
- * Version: 1.16
- * donation link: 
+ * Version: 1.17
 --]]
 
 --[[
  * Changelog:
+ * v1.17 (2026-08-21)
+    + update for newer Fiddler actions
  * v1.16 (2023-11-28)
     + another minor improvement to transpose function
  * v1.15 (2023-11-28)
@@ -131,7 +132,8 @@ function MIDINotesInRE(task)
   if task == 9 or 10 then                -- if task is mouse-related,
     mouseNote, mouseTake, mouseItem, mouse_position_ppq = getMouseInfo()
   end
-                  -- if it's not MIDI, or of there's no item under cursor, quit everything
+  
+  -- if it's not MIDI, or of there's no item under cursor, quit everything
   --if mouseTake ~= nil and reaper.TakeIsMIDI(mouseTake) == 0 then return end      
   
   if RazorEditSelectionExists(1, 1) then      -- if no razor edit, create one out of selected item                                         -- if no item is selected, select item under mouse 
